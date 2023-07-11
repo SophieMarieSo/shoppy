94fc25ae973ae1aa5f513b9c53b554e465c41fa3
AhnEunJin <eunjin.an@datastreams.co.kr>
2023-07-11 17:04:35 +0900

    admin

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -6,6 +6,7 @@ import {
   signOut,
   onAuthStateChanged,
 } from 'firebase/auth';
+import { getDatabase, ref, child, get } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -17,6 +18,10 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
+const database = getDatabase(app);
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
 
 export function login() {
   signInWithPopup(auth, provider).catch(console.error);
@@ -27,7 +32,22 @@ export function logout() {
 }
 
 export function onUserStateChange(calllback) {
-  onAuthStateChanged(auth, (user) => {
-    calllback(user);
+  onAuthStateChanged(auth, async (user) => {
+    // 1. 사용자가 있는 경우 (로그인한 경우)
+    const updatesUser = user ? await adminUser(user) : null;
+    calllback(updatesUser);
+  });
+}
+
+async function adminUser(user) {
+  // 2. 사용자가 어드민 권한을 가지고 있는지 확인
+  // 3. {...user, isAdmin: true/false}
+  return get(ref(database, 'admins')).then((snapshot) => {
+    if (snapshot.exists()) {
+      const admins = snapshot.val();
+      const isAdmin = admins.includes(user.uid);
+      return { ...user, isAdmin };
+    }
+    return user;
   });
 }
